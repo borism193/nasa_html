65e77ddf37be7a314d04a7eed606d66f37c7d4b5
Boris Miranda <Boris.miranda193@gmail.com>
2021-03-25 15:55:26 -0300

    inicio html

--- nasa.rb
+++ nasa.rb
@@ -2,7 +2,8 @@ require 'uri'
 require 'net/http'
 require 'openssl'
 require 'json'
-url_n = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=0&'
+
+url_n = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=0'
 key = 'sGjBghxX5smgQFWXgCX4pc4qVfFLL4cHwcVabceH'
 
 
@@ -17,4 +18,14 @@ def pedir_datos(direccion,llave)
     return JSON.parse(response.read_body)
 end
 
-puts pedir_datos(url_n,key)
+puts 
+
+def buid_web_page(fotos)
+   html = "<html>\n<head>\n</head>\n<body>\n<ul>\n"
+   html_f ="#{html}</ul>\n</body>\n</html>"
+
+end
+
+json = pedir_datos(url_n,key)
+fotos = buid_web_page(json)
+File.write('fotos_nasa.html', fotos)
