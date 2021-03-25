568bed9bdb64a96174e942fdc2c79d62a72d553f
Boris Miranda <Boris.miranda193@gmail.com>
2021-03-25 14:56:39 -0300

    prueba uri

--- nasa.rb
+++ nasa.rb
@@ -1,1 +1,17 @@
-prueba
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+def pedir_datos(direccion)
+    url = URI(direccion)
+    https = Net::HTTP.new(url.host, url.port)
+    request = Net::HTTP::Get.new(url)
+    https.use_ssl = true
+    https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    response = https.request(request)
+
+    return JSON.parse(response.read_body)
+end 
+
+puts pedir_datos('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY')
